f926b648c03b61f87309cd010172f05f8d30f16b
Adilet Buytukov <abuytukov@gmail.com>
2024-01-13 18:36:08 +0600

    Добавлены CSS стили.

--- src/Components/ItemList.tsx
+++ src/Components/ItemList.tsx
@@ -5,16 +5,18 @@ interface ItemsListProps {
   onAdd: (itemName: string) => void;
 }
 
-const ItemsList: React.FC<ItemsListProps> = ({ items, onAdd }) => {
+const ItemsList: React.FC<ItemsListProps> = ({items, onAdd}) => {
   return (
     <div>
       <h2>Добавить в заказ:</h2>
-      {items.map((item) => (
-        <button className='item-btn' key={item.name} onClick={() => onAdd(item.name)}>
-          <img src={item.image} alt=""/>
-          {item.name} {item.price} Сом
-        </button>
-      ))}
+      <div className='flex-btn'>
+        {items.map((item) => (
+          <button className='item-btn' key={item.name} onClick={() => onAdd(item.name)}>
+            <img className='item-img' src={item.image} alt=""/>
+            {item.name} {item.price} Сом
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
